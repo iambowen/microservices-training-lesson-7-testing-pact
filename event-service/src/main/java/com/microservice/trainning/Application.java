9414ffc4296997bf17d80612df7531ffd2e66ef7
Bowen Ma <iambowen.m@gmail.com>
2017-02-06 05:50:45 +0800

    update prometheus metrics endpoint

--- event-service/src/main/java/com/microservice/trainning/Application.java
+++ event-service/src/main/java/com/microservice/trainning/Application.java
@@ -3,6 +3,15 @@ package com.microservice.trainning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.prometheus.client.Collector;
+import io.prometheus.client.exporter.MetricsServlet;
+import io.prometheus.client.hotspot.DefaultExports;
+import io.prometheus.client.spring.boot.SpringBootMetricsCollector;
+import java.util.Collection;
+import org.springframework.boot.actuate.endpoint.PublicMetrics;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,4 +19,18 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+     @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        DefaultExports.initialize();
+        return new ServletRegistrationBean(new MetricsServlet(), "/prometheus");
+    }
+
+  @Bean
+  public SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
+    SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(
+        publicMetrics);
+    springBootMetricsCollector.register();
+    return springBootMetricsCollector;
+  }
 }
